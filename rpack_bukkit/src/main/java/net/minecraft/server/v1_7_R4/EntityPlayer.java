34303a2d1b18cb6e2d35e74daf6b8531f52f8fe8
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-09-16 18:09:48 +0300

    fake NMS

---
+++ rpack_bukkit/src/main/java/net/minecraft/server/v1_7_R4/EntityPlayer.java
@@ -0,0 +1,9 @@
+package net.minecraft.server.v1_7_R4;
+
+/**
+ * Fake nms
+ */
+public final class EntityPlayer extends Entity {
+	public PlayerConnection playerConnection;
+	public String locale;
+}
---
+++ rpack_bukkit/src/main/java/net/minecraft/server/v1_8_R3/EntityPlayer.java
@@ -0,0 +1,9 @@
+package net.minecraft.server.v1_8_R3;
+
+/**
+ * Fake nms
+ */
+public final class EntityPlayer extends Entity {
+	public PlayerConnection playerConnection;
+	public String locale;
+}
